918226a63dbebcf4d14b1a549447d23e280d7b16
Kenneth Chow <kennethjchow@gmail.com>
2021-01-14 19:30:54 -0800

    red gradient with blue button

--- src/FrontPage/FrontPage.js
+++ src/FrontPage/FrontPage.js
@@ -1,16 +1,16 @@
 import "./FrontPage.css";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import RefreshOutlinedIcon from '@material-ui/icons/RefreshOutlined';
 
 const useStyles = makeStyles({
    root: {
-      background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+      background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
       border: 0,
-      borderRadius: 3,
-      boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+      boxShadow: "0 3px 5px 2px rgba(33, 203, 243, .3)",
       color: "white",
-      height: 48,
-      padding: "0 30px",
+      padding: "6px 2px",
+      margin: "6px 6px"
    },
 });
 
@@ -21,8 +21,7 @@ function FrontPage() {
          <h1 className="question-elements">Question of the day:</h1>
          <br/>
          <h3 className="question-elements">What is the point of life?</h3>
-         <br/>
-         <Button className={classes.root}>Refresh</Button>
+         <Button className={classes.root}><RefreshOutlinedIcon></RefreshOutlinedIcon></Button>
       </div>
    );
 }
